f96b9518cb751e3cce48e344fc73fd2e5e51314f
miladimos <milad.jafari6210@gmail.com>
2020-08-31 02:32:57 -0700

    Add config/config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Directory
+    |--------------------------------------------------------------------------
+    |
+    | By default this package will look in the application's 'Helpers'
+    | directory. However, you may choose to override the directory.
+    |
+    */
+
+    'directory' => 'Helpers',
+
+];
