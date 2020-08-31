bb8b58b47d2c5dffd5f6cb7b379721c7cb2358a9
miladimos <milad.jafari6210@gmail.com>
2020-08-31 02:17:48 -0700

    Initial Commit

---
+++ src/HelpersServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Miladimos\Helpers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpersServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        if($this->app->runningInConsole()) {
+//            $this->publishes([
+//                __DIR__ . '../config/config.php' => config_path('config.php'),
+//            ], 'config');
+//
+//            $this->publishes([
+//                __DIR__ . '/Helpers/helpers.php' => base_path('Helpers/helpers.php'),
+//            ], 'helpers');
+//        }
+
+//
+//        if (file_exists(base_path('Helpers/helpers.php'))) {
+//            require_once(base_path('Helpers/helpers.php'));
+//        }
+    }
+
+//    public function registerHelpers()
+//    {
+//        // Load the helpers in app/Http/helpers.php
+//        if (file_exists($file = app_path('Http/helpers.php')))
+//        {
+//            require $file;
+//        }
+//    }
+}
