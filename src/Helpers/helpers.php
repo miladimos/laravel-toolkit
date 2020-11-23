147716020b5dabbf3c3d7a053fe489f5937e3536
Milad <31257147+miladimos@users.noreply.github.com>
2020-11-23 11:54:54 +0330

    Update helpers.php

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -20,6 +20,28 @@ if(!function_exists("convertToWestern")) {
     }
 }
 
+function getUserIP()
+{
+	$client  = @$_SERVER['HTTP_CLIENT_IP'];
+	$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+	$remote  = $_SERVER['REMOTE_ADDR'];
+
+	if(filter_var($client, FILTER_VALIDATE_IP))
+	{
+		$ip = $client;
+	}
+	elseif(filter_var($forward, FILTER_VALIDATE_IP))
+	{
+		$ip = $forward;
+	}
+	else
+	{
+		$ip = $remote;
+	}
+
+	return $ip;
+}
+
 if(!function_exists("createOPTCode")) {
     function createOPTCode()
     {
