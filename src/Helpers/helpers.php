a790aba0558b512604bbadaf0bdafcf12f76a96a
Milad <31257147+miladimos@users.noreply.github.com>
2020-11-23 11:37:15 +0330

    Add some Helpers

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -101,4 +101,41 @@ if(!function_exists('decodeBase64File')) {
     }
 }
 
+/**
+ * Returns the hashed string for the given id
+ * 
+ * @param integer $id id which has to be encoded
+ * 
+ * @return string
+ */
+function encode(int $id) : string
+{
+    return Hashids::encode($id);
+}
+
+/**
+ * Returns the integer value for the given hashed string
+ *
+ * @param string $code code which has to be decoded
+ *
+ * @return integer
+ */
+function decode(string $code) : int
+{
+    return collect(Hashids::decode($code))->first();
+}
+
+/**
+ * Returns the status options
+ * 
+ * @return array
+ */
+function getStatusOptions() : array
+{
+    return [
+        '1' => 'Enabled',
+        '0' => 'Disabled'
+    ];
+}
+
 
