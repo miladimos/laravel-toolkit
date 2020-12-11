c5c748e75ebbcdbf9326c1cd4ecb9340ce32dfe9
Milad <31257147+miladimos@users.noreply.github.com>
2020-12-11 19:21:59 +0330

    Update helpers.php

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Str;
 
+if(!function_exists("isActive")) {
+    function isActive($key, $class = 'active')
+    {
+ 	if(is_array($key)) {
+		return in_array(Route::currentRouteName(), $key) ? $class : '';
+	}
+        return Route::currentRouteName() == $key ? $class : '';
+    }
+}
+
 if(!function_exists("convertToEastern")) {
     function convertToEastern($string)
     {
